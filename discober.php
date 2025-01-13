24222aa1314777aa07eb0943521571d5d4098c0f
Ivan D.S. <ivandiazsanchez1996@gmail.com>
2025-01-13 19:01:33 +0100

    Login cambiado para que sea sin AJAX y página de Discober comenzada
    Archivos modificados/añadidos:
    
    - discober.php
    - login.php
    - index.php
    
    Carpetas añadidas:
    
    -Ninguna carpeta añadida
    
    Acciones hechas:
    
    - La página de Discober tiene la estructura básica
    - Por comentario de Xavi, se ha cambiado el Inicio para que redirija a Login o Discober según si se esta logeado o no.
    - Toda la lógica de login se ha pasado a login.php y ya no se usa AJAX si no un simple form con las peticiones a BBDD en el mismo php

--- discober.php
+++ discober.php
@@ -6,15 +6,31 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/style.css?t=<?php echo time(); ?>">
     <script src="/js/jquery-3.7.1.min.js"></script>
-    <title>Affinity</title>
+    <title>Descubrir</title>
 </head>
 
-<body>
+<body id="bodyDiscober">
     <script>
         <?php if (!isset($_COOKIE['loggedUser'])) { ?>
             window.location.href = "/";
         <?php } ?>
     </script>
+    
+    <header id="headerDiscober">
+        <h2>LOGO TEXT</h2>
+    </header>
+
+    <main id="mainDiscober">
+        <div id="matchDiscober">
+            
+        </div>
+    </main>
+
+    <footer id="footer">
+        <h3>Descobrir</h3>
+        <h3>Missatges</h3>
+        <h3><a href="profile.php">Perfil</a></h3>
+    </footer>
 </body>
 
 </html>
