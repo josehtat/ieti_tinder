b2c0ec4a757bba8908ca9b09e4c7673a0b23417a
josehtat <jhurtadot33@gmail.com>
2025-01-24 12:55:54 +0100

    solucion a la recarga despues de agregar una imagen en photos.php

--- fetch_messages.php
+++ fetch_messages.php
@@ -1,5 +1,4 @@
 <?php
-// Configurar la conexión a la base de datos
 try {
     $hostname = "localhost";
     $dbname = "ieti_tinder";
@@ -11,13 +10,10 @@ try {
     exit;
 }
 
-// Obtener el mail del receptor desde la solicitud GET
 $mail_receptor = $_GET['mail'];
 
-// Obtener el mail del usuario logueado desde las cookies
 $user_email = $_COOKIE['loggedUser'];
 
-// Obtener la imagen del receptor
 $query = "SELECT p.path FROM pictures p JOIN users u ON p.email_user = u.email_user WHERE u.email_user = :mail_receptor";
 $stmt = $pdo->prepare($query);
 $stmt->bindParam(':mail_receptor', $mail_receptor);
@@ -25,7 +21,6 @@ $stmt->execute();
 $row = $stmt->fetch();
 $image_path = $row['path'];
 
-// Obtener los mensajes entre el usuario logueado y el receptor
 $query = "
     SELECT id_user, message_user, date 
     FROM messages
@@ -47,15 +42,12 @@ foreach ($messages as $message):
     $current_time = time();
     $time_diff = $current_time - $message_time;
 
-    // Mostrar la fecha solo si ha pasado más de 5 minutos desde el último mensaje
     $show_date = false;
 
-    // Si es el primer mensaje o han pasado más de 5 minutos desde el último mensaje, mostramos la fecha
     if (!$last_time || $time_diff > 300) {
         $show_date = true;
     }
 
-    // Mostrar la fecha solo si se debe mostrar
     if ($show_date) {
         $formatted_date = date("d M Y, H:i", $message_time);
         echo "<div class='date'>$formatted_date</div>";
