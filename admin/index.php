c261a449ca868541612fc79b97b14d20bf5a29d9
Ivan D.S. <ivandiazsanchez1996@gmail.com>
2025-01-29 19:43:11 +0100

    Include en lugar de redireccion al archivo de error 403

--- admin/index.php
+++ admin/index.php
@@ -3,8 +3,8 @@
 if (!isset($_COOKIE['loggedUser']) || !isset($_COOKIE['userRole']) || $_COOKIE['userRole'] !== 'admin') {
     // Redirigir al login si no está autenticado como administrador
     http_response_code(403);
-    header("Location: ../error/403.php");
-    die("Error 403: Prohibido");
+    include("../errors/403.php");
+    die();
 }
 
 // Puedes añadir un mensaje o lógica adicional aquí, si lo necesitas
